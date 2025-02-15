96d6d0e360c740b26efb093c79f3d5e746101f69
Your Name <sahafmfaisal076@gmail.com>
2025-02-16 00:07:30 +0530

    Updated interface

--- datum/src/App.js
+++ datum/src/App.js
@@ -1,22 +1,13 @@
-import logo from './icon.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Work in Progress</h1>
+        <p>This app is currently under construction. Check back soon!</p>
+        <div className="spinner"></div>
       </header>
     </div>
   );
